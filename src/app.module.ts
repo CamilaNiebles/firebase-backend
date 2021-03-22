4e1c9a77a41ffb70111ec7e7dece32f25548a2da
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-03-21 20:18:16 -0500

    First version to create users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/verifikar'),
+    UserModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [AuthService],
 })
 export class AppModule {}
