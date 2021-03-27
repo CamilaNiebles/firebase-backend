7591d9aeb03ca6f865b45bf6606797a6dca9c37a
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-03-27 14:55:58 -0500

    Configure dockerfile and docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from './forms/forms.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/verifikar'),
+    MongooseModule.forRoot('mongodb://mongo-db:27017/verifikar'),
     UserModule,
     AuthModule,
     FormsModule,
