c1a30fa70065758dea055b922b14f0bd369f58bd
danielniebles <danielf.niebles@gmail.com>
2021-03-29 20:49:42 -0500

    Create isolated code for list management

--- src/forms/forms.service.ts
+++ src/forms/forms.service.ts
@@ -16,21 +16,31 @@ export class FormsService {
   async getTemplateByName(name: string){
     const response = await this.templateRepository.getTemplateByName(name)
     const { question } = response
-    const includesList = question.filter( (question) => question['type'].includes('list') )
-    const lists = [...new Set(includesList.map((question) => question['resource']))]
+    
+    const listsArray = await this.manageListsContent(question)
+    const finalForm = this.generateFinalForm(question, listsArray)
 
-    const listsArray = await Promise.all( lists.map( (list) => {
+  return finalForm
+
+  }
+
+  manageListsContent(question: any){
+    const includesList = question.filter( (question) => question['type'].includes('list') )
+    const lists : any = [...new Set(includesList.map((question) => question['resource']))]
+    const listsArray =  Promise.all( lists.map( (list) => {
       return this.listRepository.getListByName(list)
     }))
+    return listsArray
+  }
 
+  generateFinalForm(question: any, listsArray: any){
     const finalForm = question.map( (question) => {
       const listItems = listsArray.filter( (list) => list['name'] === question['resource'])
       question['options'] = listItems.length > 0 ? listItems[0]['options'] : null
       return question
       }
     )
-
-  return finalForm 
-
+    return finalForm
   }
+
 }
