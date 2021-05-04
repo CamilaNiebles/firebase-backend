a9b929946aba8b706c294598c22e07e9ef18ca65
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-05-03 21:28:36 -0500

    Endpoint to save new rchilli record

--- src/rchilli/rchilli.controller.ts
+++ src/rchilli/rchilli.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateNewReading } from './dto/create.reading';
+import { RchilliService } from './rchilli.service';
 
 @Controller('rchilli')
-export class RchilliController {}
+export class RchilliController {
+  constructor(private rchilliService: RchilliService) {}
+
+  @Post()
+  async createNewRecord(@Body() createNew: CreateNewReading, @Res() res: any) {
+    try {
+      const response = await this.rchilliService.createRecord(createNew);
+      return res.status(HttpStatus.CREATED).send(response);
+    } catch (error) {
+      return res.status(error.status).send(error.message);
+    }
+  }
+}
