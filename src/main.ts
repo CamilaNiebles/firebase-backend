b5424171aaa07f3e8e9bae249cb2cca7bfb0d525
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-05-07 14:19:40 -0500

    Fix error in rchilli

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   const PORT = process.env.PORT || 8080;
   await app.listen(PORT);
 }
